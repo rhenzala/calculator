5300fc828f443ba3d502d511060ef728eeb258cc
zal626 <97076128+zal626@users.noreply.github.com>
2023-06-26 18:07:46 +0800

    Add basic arithmetic functions

---
+++ script.js
@@ -0,0 +1,13 @@
+function add(a, b){
+    return a + b
+}
+function sub(a, b){
+    return a - b
+}
+function prod(a, b){
+    return a * b
+}
+function quot(a, b){
+    if (b === 0) return "SYNTAX ERROR"
+    return a / b
+}
