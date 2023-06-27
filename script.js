b71134121e039ab8d1380e0aeab11b9395bc20c4
zal626 <97076128+zal626@users.noreply.github.com>
2023-06-27 19:04:43 +0800

    Revise the functionality up to populateDisplay

--- script.js
+++ script.js
@@ -1,13 +1,55 @@
-function add(a, b){
-    return a + b
+let num1;
+let num2;
+let operator = '';
+
+
+const numKey = document.querySelectorAll(".num");
+const ops = document.querySelectorAll(".ops");
+const equals = document.getElementById("equals");
+const clear = document.getElementById("clear");
+const del = document.getElementById("delete");
+const period = document.getElementById("period");
+
+const prevDisplay = document.getElementById("prev");
+const currentDisplay = document.getElementById("current");
+
+clear.addEventListener('click', () => {
+    prevDisplay.textContent = '';
+    currentDisplay.textContent = '';
+    window.location.reload();
+})
+
+numKey.forEach(btn => btn.addEventListener('click', populateDisplay)) 
+ops.forEach(btn => btn.addEventListener('click', populateDisplay)) 
+
+function populateDisplay(){
+    let x = this.value
+    currentDisplay.textContent += x
 }
-function sub(a, b){
-    return a - b
+
+
+function operate(a, b, op){
+    switch (op){
+        case '+':
+            add(a, b);
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case '×':
+            multiply(a, b);
+            break;
+        case '÷':
+            divide(a, b);
+            break;
+    }
 }
-function prod(a, b){
-    return a * b
-}
-function quot(a, b){
-    if (b === 0) return "SYNTAX ERROR"
-    return a / b
+
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => {
+    if (b === 0) return "SYNTAX ERROR";
+    return a / b;
 }
